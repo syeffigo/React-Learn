0327559a685fd6b13f5b7a01dcd5feed892214c9
syeffigo <saurabh.yadav@effigo.tech>
2024-02-27 14:40:16 +0530

    Some Changes

--- src/Products.jsx
+++ src/Products.jsx
@@ -7,7 +7,7 @@ function Products({naam, data}) {
        67 is {naam}
        <h2>{data.age}</h2>
        <h2>{data.name}</h2>
-       <h2 className={`${a == false ? "text-red-600" : "text-blue-600"}`}>{a === false ? "hello" : "hey"}</h2>
+       <h2 className={`${a == false ? "text-red-600" : "text-green-600"}`}>{a === false ? "hello" : "hey"}</h2>
        <button onClick={()=> b(!a)} className='px-3 bg-blue-500 rounded-md text-xs'>Change</button>
     </div>
   )
